694b520031ee1882c53d7381dcd1c7c8e9f4fe86
adeslauriers89 <adeslauriers89@gmail.com>
2016-02-09 12:51:03 -0800

    added a user

---
+++ Roomies2.0/RoomieUser.m
@@ -0,0 +1,13 @@
+//
+//  roomieUser.m
+//  Roomies2.0
+//
+//  Created by Adam DesLauriers on 2016-02-09.
+//  Copyright © 2016 Adam DesLauriers & Thiago Heitling. All rights reserved.
+//
+
+#import "RoomieUser.h"
+
+@implementation RoomieUser
+
+@end
